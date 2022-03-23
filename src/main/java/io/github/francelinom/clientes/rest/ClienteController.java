ebd7229627ebc894571c00d3970d059ca352d91c
francelino <fmarcilio1989@gmail.com>
2022-03-23 16:05:31 -0300

    metodo de deletar cliente

--- src/main/java/io/github/francelinom/clientes/rest/ClienteController.java
+++ src/main/java/io/github/francelinom/clientes/rest/ClienteController.java
@@ -4,6 +4,7 @@ import io.github.francelinom.clientes.model.entity.Cliente;
 import io.github.francelinom.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,18 @@ public class ClienteController {
     public Cliente acharPorId(@PathVariable Integer id) {
         return repository
                 .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
