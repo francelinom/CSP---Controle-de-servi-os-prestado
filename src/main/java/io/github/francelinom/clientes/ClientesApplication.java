4182b37b84d6bcb32caaf5a5dc8c208adbe2003a
francelino <fmarcilio1989@gmail.com>
2022-03-22 21:35:28 -0300

    Configurando o pom.xml

---
+++ src/main/java/io/github/francelinom/clientes/ClientesApplication.java
@@ -0,0 +1,10 @@
+package io.github.francelinom.clientes;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+    public static void main(String[] args) {
+
+    }
+}
