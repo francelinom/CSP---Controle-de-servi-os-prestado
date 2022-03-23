3383be1dce583ff90b4df7806691a63a921f4eb8
francelino <fmarcilio1989@gmail.com>
2022-03-22 21:45:29 -0300

    Configurando o main para iniciar o projeto

--- src/main/java/io/github/francelinom/clientes/ClientesApplication.java
+++ src/main/java/io/github/francelinom/clientes/ClientesApplication.java
@@ -1,10 +1,11 @@
 package io.github.francelinom.clientes;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientesApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ClientesApplication.class, args);
     }
 }
