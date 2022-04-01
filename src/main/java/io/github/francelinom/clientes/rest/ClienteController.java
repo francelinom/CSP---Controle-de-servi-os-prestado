19940252efeb84209b8bdbd4959f67c45969a5d0
francelino <fmarcilio1989@gmail.com>
2022-03-31 23:01:45 -0300

    adicionando tratamento de cross

--- src/main/java/io/github/francelinom/clientes/rest/ClienteController.java
+++ src/main/java/io/github/francelinom/clientes/rest/ClienteController.java
@@ -4,6 +4,7 @@ import io.github.francelinom.clientes.model.entity.Cliente;
 import io.github.francelinom.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     private final ClienteRepository repository;
